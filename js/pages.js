5cc5b0b4e0aa5e7f30b86633f3e37cdb3fc3fb51
Myumi <myumiak@gmail.com>
2019-04-05 10:17:51 -0400

    Working on adding mobile tabs

--- js/pages.js
+++ js/pages.js
@@ -1,9 +1,9 @@
-//make hours less ugly
+//make hours less ugly - maybe a dropdown?
 //fix lunch menu - combine css with dinner
 //MOBILE:
 //figure out something cute for main menu on mobile
-//change menu items for mobile
-$(document).ready(function(){
+//nav-icon - onclick() -> add class dropdown, remove class nav, on click redo
+$(document).ready(function() {
   // make home seem active & others not
   document.getElementById("home-tab").classList.add("active");
   $(".tab").css("display", "none");
@@ -12,7 +12,7 @@ $(document).ready(function(){
   //make menus invisible
   $(".menu").css("display", "none");
 
-    //display menus for dinner
+  //display menus for dinner
   $("#dinner button").click(function() {
     $("#dinner button").css("background", "var(--button-color)");
     let button = $(this);
@@ -21,20 +21,42 @@ $(document).ready(function(){
       .attr("id")
       .replace(/-btn/g, "");
     $("#dinner .menu").css("display", "none");
-    $("#" + menuName).fadeIn(300).css("display", "");
+    $("#" + menuName)
+      .fadeIn(300)
+      .css("display", "");
   });
 
   //set appetizer button as default clicked
   $("#appetizer-btn").click();
 
-    //display pages
+  //display pages
   $(".nav li").click(function() {
-    let tabName = $(this)
-      .attr("id")
-      .replace(/-tab/g, "");
+    tabShow($(this));
+  });
+
+  $("#nav-icon").click(function() {
+    if ($(".header ul").hasClass("nav")) {
+      $(".header ul").removeClass("nav");
+      $(".header ul").addClass("dropdown");
+    }
+    $(".dropdown").css("display") === "none"
+      ? $(".dropdown").css("display", "inline")
+      : $(".dropdown").css("display", "none");
+  });
+
+  //display pages mobile
+  $(".dropdown li").click(function() {
+    tabShow($(this));
+    $(".dropdown").css("display", "none");
+  });
+
+  function tabShow(tab) {
+    let tabName = tab.attr("id").replace(/-tab/g, "");
     $(".nav li").removeClass("active");
     $(this).addClass("active");
     $(".tab").css("display", "none");
-    $("#" + tabName).fadeIn(500).css("display", "");
-  });
+    $("#" + tabName)
+      .fadeIn(500)
+      .css("display", "");
+  }
 });
